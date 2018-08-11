d9adc78bc0cd6d773a2ec574d83479b3a7f865c3
arohigupta <argu2081@colorado.edu>
2018-08-11 13:59:18 -0700

    fixed quotes

---
+++ ansible_openstack/all_in_one/roles/apply_contrail_security/tasks/install_env.sh
@@ -0,0 +1,6 @@
+# @Author: Arohi Gupta <agupta>
+# @Date:   2018-08-10T14:10:44-07:00
+# @Email:  agupta@juniper.net
+# @Filename: install_env.sh
+# @Last modified by:   agupta
+# @Last modified time: 2018-08-10T14:10:44-07:00
